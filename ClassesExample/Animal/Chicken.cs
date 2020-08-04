fa484ed66d1995da184445903a258996df71de01
Monique.Bass01 <monique.bass01@gmail.com>
2020-08-04 17:12:56 -0500

    Add project files.

---
+++ ClassesExample/Animal/Chicken.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExample.Animal
+{
+    class Chicken
+    {
+
+        //property
+        public string Name { get; set; }
+
+        //Field
+        private bool _isHungry = true;
+
+        //default constructor
+        public Chicken(string name)
+        {
+            Name = name;
+            Console.WriteLine("Chicken got instantiated...");
+        }
+        //method (signature = accessibility modifier -> return type -> Nme -> Parameters
+        public void Peck(string typeOfFood)
+        {
+            if (_isHungry)
+            {
+                Console.WriteLine($"{Name} pecks hungryily at the {typeOfFood}.");
+                _isHungry = false;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is not hungry.");
+            }
+        }
+    }
+}
---
+++ ClassesExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassesExample.Animal;
+using System;
+
+namespace ClassesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chicken = new Chicken("Liza");
+            
+            chicken.Peck("corn");
+            
+            var bob = new Chicken("Bob");
+            
+            bob.Peck("watermelon");
+            
+        }
+    }
+}
